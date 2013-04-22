f3ad336841541f15f12dc61031670f099c87a9bf
Vitaly Serov <serovvitaly@gmail.com>
2013-04-22 13:44:45 +0400

    первый коммит

---
+++ js/custom.js
@@ -0,0 +1,87 @@
+$(document).ready(function(){
+
+	$('.carousel').carousel({
+	    interval: 12000
+  	})
+
+	/* задаем количество слайдов в слайдере */
+	var win_width = $(window).width();
+		$(".sliderkit").sliderkit({
+	    shownavitems:4,
+	    scroll:1,
+	    auto:false,
+	    circular:false
+	  });
+
+	if (win_width >= 1200) {
+	  $('#carousel-main').sliderkit({
+	      shownavitems:4,
+	      auto:false
+	  });
+	} else if (979 < win_width && win_width < 1200) {
+	  $('#carousel-main').sliderkit({
+	      shownavitems:4,
+	      auto:false
+	  });
+	} else  if (768 < win_width && win_width < 979) {
+	  $('#carousel-main').sliderkit({
+	      shownavitems:3,
+	      auto:false
+	  });
+	} else if (480 < win_width && win_width < 768) {
+	  $('#carousel-main').sliderkit({
+	      shownavitems:2,
+	      auto:false
+	  });
+	} else if (win_width < 480) {
+	  $('#carousel-main').sliderkit({
+	      shownavitems:1,
+	      auto:false
+	  });
+	}
+
+	function hexFromRGB(r, g, b) {
+		var hex = [
+			r.toString( 16 )
+		];
+		$.each( hex, function( nr, val ) {
+			if ( val.length === 1 ) {
+				hex[ nr ] = "0" + val;
+			}
+		});
+		return hex.join( "" ).toUpperCase();
+	}
+	function refreshSwatch() {
+		var red = $( ".slider" ).slider( "value" ),
+			hex = hexFromRGB( red );
+	}
+	
+	$( ".slider" ).slider({
+		orientation: "horizontal",
+		range: "min",
+		max: 100,
+		value: 50
+	});
+	
+	$( ".slider.sliderType1" ).slider( "value", 60 );
+	$( ".slider.sliderType2" ).slider( "value", 0 );
+
+	//$.datepicker.setDefaults( $.datepicker.regional[ "ru" ] );
+	$( ".datepicker" ).datepicker();
+
+	$('.datepicker .ui-state-default').append('<span class="grad"></span>');
+
+	$('.calend .btn').click(function(){
+		$(this).parent().parent().find('.calendar').toggle();
+	});
+
+	$('.dynDateGraph li').hover(function(){
+		$('.dynDateGraph').find('div.price').remove();
+		$('.dynDateGraph li.active').removeClass('active');
+		$(this).addClass('active').prepend('<div class="price">230 $</div>');
+	})
+
+	$('#regOpen').click(function(){
+		$('.regOn, unReg').toggle();
+	});
+})
